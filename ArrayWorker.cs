25afc49e3018b963582fe4b23277d932aa4a91ea
VGolovatiy <92428017+VGolovatiy@users.noreply.github.com>
2021-10-27 19:11:57 +0300

    Add files via upload

---
+++ ArrayWorker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class ArrayWorker
+
+    {
+        public void InvMass(int[] arr)
+        {
+            Array.Reverse(arr);
+        }
+        
+        public void InvMass2(ref int[] arr, int newValue, int index)
+        {
+            int[] newArray = new int[arr.Length + 1];
+            newArray[index] = newValue;
+            for (int i = 0; i < index; i++)
+                newArray[i] = arr[i];
+            for (int i = index; i < arr.Length; i++)
+                newArray[i+1] = arr[i];
+            arr = newArray;
+
+
+        }
+        
+    }
+    public class Cone
+    {
+        double radius;
+        double height;
+        double pi = Math.PI;
+        double result;
+        public Cone(int r, int h)
+        {
+            radius = r;
+            height = h;
+        }
+        public double PlPovOsn(int r, int h)
+        {
+            result = 2 + pi + r * h;
+            return result;
+        }
+        public double PolnPl(int r, int h)
+        {
+            result = 2 * pi * r * (r + h);
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassLibrary;
+
+namespace ConsoleApp5_libl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+            int newValue = 5;
+            ArrayWorker aw = new ArrayWorker();
+            int[] myArray = new int[] { 4, 8, 15, 16, 23, 42 };
+            aw.InvMass(myArray);
+            aw.InvMass2(ref myArray, newValue, index);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
